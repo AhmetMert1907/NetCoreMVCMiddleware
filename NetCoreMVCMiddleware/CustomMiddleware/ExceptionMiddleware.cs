669d7da7b5576c7be2f2a492637df761d0abc097
kurumsal <kurumsal@KDK-104-PC1-EGT>
2022-06-11 18:17:23 +0300

    Add project files.

---
+++ NetCoreMVCMiddleware/CustomMiddleware/ExceptionMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreMVCMiddleware.CustomMiddleware
+{
+    public class ExceptionMiddleware
+    {
+        private RequestDelegate _next;
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            //logic action 
+            int a = 5;
+            await _next(context);
+        }
+    }
+}
---
+++ NetCoreMVCMiddleware/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetCoreMVCMiddleware.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
